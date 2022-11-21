8456f3065bb858b7bbb8322f673c9218129ecc6e
Tom Jaroli <tamas.jaroli@gmail.com>
2022-11-22 01:12:21 +0200

    Add more options

--- lua/user/core/options.lua
+++ lua/user/core/options.lua
@@ -1,37 +1,54 @@
 local opt = vim.opt -- for conciseness
 
 local options = {
-    -- line numbers
-    relativenumber = true,
-    number = true,
-    -- tabs & indentation
-    tabstop = 4,
-    shiftwidth = 4,
+    clipboard = "unnamedplus", -- allows neovim to access the system clipboard
+    cmdheight = 1, -- more space in the neovim command line for displaying messages
+    completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+    conceallevel = 0, -- so that `` is visible in markdown files
+    fileencoding = "utf-8", -- the encoding written to a file
+    hlsearch = true, -- highlight all matches on previous search pattern
+    ignorecase = true, -- ignore case in search patterns
+    mouse = "a", -- allow the mouse to be used in neovim
+    pumheight = 10, -- pop up menu height
+    smartcase = true, -- smart case
+    smartindent = true, -- make indenting smarter again
+    splitbelow = true, -- force all horizontal splits to go below current window
+    splitright = true, -- force all vertical splits to go to the right of current window
+    termguicolors = true, -- set term gui colors (most terminals support this)
+    timeoutlen = 1000, -- time to wait for a mapped sequence to complete (in milliseconds)
+    updatetime = 100, -- faster completion (4000ms default)
+    expandtab = true, -- convert tabs to spaces
+    shiftwidth = 4, -- the number of spaces inserted for each indentation
+    tabstop = 4, -- insert 2 spaces for a tab
     softtabstop = 0,
-    expandtab = true,
     smarttab = true,
+    cursorline = true, -- highlight the current line
     autoindent = true,
-    -- line wrapping
-    wrap = false,
-    smartcase = true,
-    -- cursor line
-    cursorline = true,
-    -- appearance
-    termguicolors = true,
-    background = "dark",
-    signcolumn = "yes",
-    -- backspace
+    number = true, -- set numbered lines
+    laststatus = 3,
+    relativenumber = true, -- set relative numbered lines
+    numberwidth = 4, -- set number column width to 2 {default 4}
+    signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
+    wrap = false, -- display lines as one long line
+    scrolloff = 8, -- is one of my fav
+    sidescrolloff = 8,
+    title = true,
     backspace = "indent,eol,start",
-    -- split windows
-    splitright = true,
-    splitbelow = true,
 }
 
 for key, value in pairs(options) do
     opt[key] = value
 end
 
--- clipboard
-opt.clipboard:append("unnamedplus")
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.fillchars.eob = " "
+vim.opt.shortmess:append "c"
 
-opt.iskeyword:append("-")
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
+
+-- clipboard
+opt.clipboard:append "unnamedplus"
+
+opt.iskeyword:append "-"
