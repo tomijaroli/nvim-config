caa0376b90ec1f829afbd028616149ea9f24bf52
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-03-19 00:39:53 +0200

    Revamp completion.lua

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,34 +1,43 @@
 return {
     {
-        "hrsh7th/cmp-nvim-lsp", -- nvim-cmp source for neovim's built-in LSP
-        version = "v.0.0.1",
-    },
-    {
-        "hrsh7th/cmp-buffer", -- nvim-cmp source for buffer words
-        commit = "3022dbc9166796b644a841a02de8dd1cc1d311fa",
-    },
-    {
-        "hrsh7th/cmp-path",
-        commit = "91ff86cd9c29299a64f968ebb45846c485725f23",
-    },
-    {
-        "L3MON4D3/LuaSnip",
-        version = "2.2.0",
+        "hrsh7th/nvim-cmp",
+        version = "v0.0.1",
         dependencies = {
+            {
+                "L3MON4D3/LuaSnip",
+                version = "2.2.0",
+                build = (function()
+                    if vim.fn.has "win32" == 1 or vim.fn.executable "make" == 0 then
+                        return
+                    end
+                    return "make install_jsregexp"
+                end)(),
+                dependencies = {
+                    {
+                        "rafamadriz/friendly-snippets",
+                        commit = "dcd4a586439a1c81357d5b9d26319ae218cc9479",
+                        config = function()
+                            require("luasnip.loaders.from_vscode").lazy_load()
+                        end,
+                    },
+                },
+            },
             {
                 "saadparwaiz1/cmp_luasnip",
                 commit = "05a9ab28b53f71d1aece421ef32fee2cb857a843",
             },
             {
-                "rafamadriz/friendly-snippets",
-                commit = "dcd4a586439a1c81357d5b9d26319ae218cc9479",
+                "hrsh7th/cmp-nvim-lsp", -- nvim-cmp source for neovim's built-in LSP
+                version = "v.0.0.1",
             },
-        },
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        version = "v0.0.1",
-        dependencies = {
+            {
+                "hrsh7th/cmp-buffer", -- nvim-cmp source for buffer words
+                commit = "3022dbc9166796b644a841a02de8dd1cc1d311fa",
+            },
+            {
+                "hrsh7th/cmp-path",
+                commit = "91ff86cd9c29299a64f968ebb45846c485725f23",
+            },
             {
                 "onsails/lspkind.nvim",
                 commit = "1735dd5a5054c1fb7feaf8e8658dbab925f4f0cf",
@@ -36,35 +45,39 @@ return {
         },
         config = function()
             local cmp = require "cmp"
+            local luasnip = require "luasnip"
             local lspkind = require "lspkind"
-            require("luasnip.loaders.from_vscode").lazy_load()
 
-            vim.opt.completeopt = "menu,menuone,noselect"
-            vim.cmd [[
-                highlight! default link CmpItemKind CmpItemMenuDefault
-            ]]
+            luasnip.config.setup {}
 
             cmp.setup {
                 snippet = {
                     expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
+                        luasnip.lsp_expand(args.body)
                     end,
                 },
+                completion = { completeopt = "menu,menuone,noinsert" },
                 window = {
                     completion = cmp.config.window.bordered(),
                     documentation = cmp.config.window.bordered(),
                 },
                 mapping = cmp.mapping.preset.insert {
-                    ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-                    ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+                    ["<C-p>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+                    ["<C-n>"] = cmp.mapping.select_next_item(), -- next suggestion
                     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-space>"] = cmp.mapping.complete(), -- show completion suggestion
-                    ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-                    ["<CR>"] = cmp.mapping.confirm {
-                        behavior = cmp.ConfirmBehavior.Replace,
-                        select = false,
-                    },
+                    ["<C-space>"] = cmp.mapping.complete {}, -- show completion suggestion
+                    ["<C-y>"] = cmp.mapping.confirm { select = false },
+                    ["<C-l>"] = cmp.mapping(function()
+                        if luasnip.expand_or_locally_jumpable() then
+                            luasnip.expand_or_jump()
+                        end
+                    end, { "i", "s" }),
+                    ["<C-h>"] = cmp.mapping(function()
+                        if luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        end
+                    end, { "i", "s" }),
                 },
                 sources = cmp.config.sources {
                     { name = "nvim_lsp" },
