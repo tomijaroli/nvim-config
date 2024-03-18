b8f158dbfb9f9ce79f4fc41e2e2d4f96d3dc04f2
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-03-18 22:39:21 +0200

    Revamp which-key config

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,8 +1,17 @@
 return {
     "folke/which-key.nvim",
     version = "v1.6.0",
+    event = "VimEnter",
     config = function()
         local which_key = require "which-key"
         which_key.setup()
+
+        which_key.register {
+            ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+            ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+            ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+            ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+            ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+        }
     end,
 }
