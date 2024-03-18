ef55a501bf5bc7464b1f504f4fa4769ac9e1bcd8
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-03-19 00:24:03 +0200

    Switch from none-ls to conform

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,54 +1,25 @@
 return {
     {
-        "nvimtools/none-ls.nvim",
-        commit = "72e25ed4162474ef5d666525853f8a42bffd97c5",
-        dependencies = {
-            {
-                "nvimtools/none-ls-extras.nvim",
-                commit = "c226aca7a506dd4325fb96188bffe804d24bbf18",
+        "stevearc/conform.nvim",
+        version = "v5.4.0",
+        opts = {
+            notify_on_error = false,
+            format_on_save = function(bufnr)
+                local disable_filetypes = { c = true, cpp = true }
+                return {
+                    timeout_ms = 500,
+                    lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+                }
+            end,
+            -- TODO: fix ruby formatting and add swift
+            formatters_by_ft = {
+                lua = { "stylua" },
+                javascript = { { "prettierd", "prettier" } },
+                python = { "isort", "black" },
+                ruby = { "rubocop" },
+                yaml = { "yamlls" },
             },
         },
-        config = function()
-            local null_ls = require "null-ls"
-            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-            null_ls.setup {
-                sources = {
-                    null_ls.builtins.formatting.stylua,
-                    null_ls.builtins.formatting.rubocop,
-                    null_ls.builtins.diagnostics.rubocop,
-                    null_ls.builtins.formatting.prettier,
-                    require "none-ls.diagnostics.eslint_d",
-                },
-                on_attach = function(current_client, bufnr)
-                    if current_client.supports_method "textDocument/formatting" then
-                        vim.api.nvim_clear_autocmds {
-                            group = augroup,
-                            buffer = bufnr,
-                        }
-                        vim.api.nvim_create_autocmd("BufWritePre", {
-                            group = augroup,
-                            buffer = bufnr,
-                            callback = function()
-                                vim.lsp.buf.format {
-                                    filter = function(client)
-                                        return client.name == "null-ls"
-                                    end,
-                                    bufnr = bufnr,
-                                }
-                            end,
-                        })
-                    end
-                end,
-            }
-
-            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format code" })
-        end,
-    },
-    {
-        "jayp0521/mason-null-ls.nvim",
-        version = "v2.5.1",
-        ensure_installed = { "prettier", "stylua", "swift-mesonlsp", "yamlls" },
     },
     {
         "gennaro-tedesco/nvim-jqx",
