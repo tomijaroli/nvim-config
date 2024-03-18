af55340b1f4ee8d6f02d1c4db2bd9f3c6d365a3a
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-03-18 22:38:10 +0200

    Disable dap config

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,26 +1,26 @@
 return {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-        "rcarriga/nvim-dap-ui",
-    },
-    config = function()
-        local dap = require "dap"
-        local dapui = require "dapui"
-
-        vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
-        vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "Continue debugger" })
-
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
-    end,
+    -- "mfussenegger/nvim-dap",
+    -- dependencies = {
+    --     "rcarriga/nvim-dap-ui",
+    -- },
+    -- config = function()
+    --     local dap = require "dap"
+    --     local dapui = require "dapui"
+    --
+    --     vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+    --     vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "Continue debugger" })
+    --
+    --     dap.listeners.before.attach.dapui_config = function()
+    --         dapui.open()
+    --     end
+    --     dap.listeners.before.launch.dapui_config = function()
+    --         dapui.open()
+    --     end
+    --     dap.listeners.before.event_terminated.dapui_config = function()
+    --         dapui.close()
+    --     end
+    --     dap.listeners.before.event_exited.dapui_config = function()
+    --         dapui.close()
+    --     end
+    -- end,
 }
