5b94258432e59d4988326c83b0162cd73fab2122
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-03-18 22:36:21 +0200

    Refactor and clean up options.lua

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,43 +2,30 @@ vim.cmd "autocmd!"
 local opt = vim.opt -- for conciseness
 
 local options = {
-    clipboard = "unnamedplus", -- allows neovim to access the system clipboard
-    cmdheight = 1, -- more space in the neovim command line for displaying messages
-    completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-    conceallevel = 0, -- so that `` is visible in markdown files
-    fileencoding = "utf-8", -- the encoding written to a file
-    hlsearch = true, -- highlight all matches on previous search pattern
-    ignorecase = true, -- ignore case in search patterns
-    mouse = "a", -- allow the mouse to be used in neovim
-    pumheight = 10, -- pop up menu height
-    smartcase = true, -- smart case
-    smartindent = true, -- make indenting smarter again
-    splitbelow = true, -- force all horizontal splits to go below current window
-    splitright = true, -- force all vertical splits to go to the right of current window
-    termguicolors = true, -- set term gui colors (most terminals support this)
-    timeoutlen = 1000, -- time to wait for a mapped sequence to complete (in milliseconds)
-    updatetime = 100, -- faster completion (4000ms default)
-    expandtab = true, -- convert tabs to spaces
-    shiftwidth = 4, -- the number of spaces inserted for each indentation
-    tabstop = 4, -- insert 2 spaces for a tab
-    softtabstop = 0,
-    smarttab = true,
-    cursorline = true, -- highlight the current line
-    autoindent = true,
     number = true, -- set numbered lines
-    laststatus = 3,
     relativenumber = true, -- set relative numbered lines
-    numberwidth = 4, -- set number column width to 2 {default 4}
-    signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
-    wrap = false, -- display lines as one long line
+    mouse = "a", -- allow the mouse to be used in neovim
+    showmode = false, -- no need to show mode, there's a statusline already
+    clipboard = "unnamedplus", -- allows neovim to access the system clipboard
     breakindent = true,
-    scrolloff = 8, -- is one of my fav
+    undofile = true,
+    ignorecase = true, -- ignore case in search patterns
+    smartcase = true, -- smart case
+    signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
+    updatetime = 250, -- faster completion
+    timeoutlen = 300, -- time to wait for a mapped sequence to complete (in milliseconds)
+    splitright = true, -- force all vertical splits to go to the right of current window
+    splitbelow = true, -- force all horizontal splits to go below current window
+    list = true,
+    listchars = { tab = "» ", trail = "·", nbsp = "␣" },
+    inccommand = "split",
+    cursorline = true, -- highlight the current line
+    scrolloff = 10,
+    hlsearch = true, -- highlight all matches on previous search pattern
+    termguicolors = true, -- set term gui colors (most terminals support this)
     sidescrolloff = 8,
     title = true,
     backspace = "indent,eol,start",
-    shell = "zsh",
-    inccommand = "split",
-    wildoptions = "pum",
     pumblend = 10,
     fcs = "eob: ",
 }
@@ -48,9 +35,7 @@ for key, value in pairs(options) do
 end
 
 local appends = {
-    shortmess = "c",
-    path = { "**" },
-    clipboard = "unnamedplus",
+    -- path = { "**" },
     iskeyword = "-",
 }
 
@@ -58,12 +43,9 @@ for key, value in pairs(appends) do
     opt[key]:append(value)
 end
 
-vim.scriptencoding = "utf-8"
 vim.wo.number = true
 
 opt.fillchars.eob = " "
 
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
-
--- vim.highlight.create("Comment", { cterm = "italic", gui = "italic" }, true)
