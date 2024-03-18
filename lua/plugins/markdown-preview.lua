cb9ef379db2d25052f531f1b8f0bd99f052d0f38
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-03-18 23:59:28 +0200

    Cleanup markdown-preview.lua

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -2,17 +2,10 @@ return {
     {
         "iamcco/markdown-preview.nvim",
         version = "v0.0.10",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
         build = function()
             vim.fn["mkdp#util#install"]()
-        end,
-        config = function()
-            local status, markdown = pcall(require, "markdown-preview")
-            if not status then
-                print "Markdown Preview not found"
-                return
-            end
-
-            markdown.setup()
         end,
     },
 }
