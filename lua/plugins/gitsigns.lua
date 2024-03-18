7ce11b06001dd8e4f60b8a760013db8bc72cf300
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-03-18 22:39:06 +0200

    Revamp gitsigns config

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,7 +1,13 @@
 return {
     "lewis6991/gitsigns.nvim",
     version = "v0.7",
-    config = function()
-        require("gitsigns").setup()
-    end,
+    opts = {
+        signs = {
+            add = { text = "+" },
+            change = { text = "~" },
+            delete = { text = "_" },
+            topdelete = { text = "‾" },
+            changedelete = { text = "~" },
+        },
+    },
 }
