6bb38e9aac932c78ffb766d2a19ad0e55396fb21
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-03-21 22:31:49 +0200

    Add hidden file search to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -42,9 +42,16 @@ return {
             vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
             vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
             vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+            vim.keymap.set("n", "<leader>sF", "<cmd>Telescope find_files hidden=true<CR>")
             vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
             vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
             vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+            vim.keymap.set(
+                "n",
+                "<leader>sG",
+                "<cmd>Telescope live_grep hidden=true<CR>",
+                { desc = "[S]earch by [G]rep including hidden" }
+            )
             vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
             vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
             vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
