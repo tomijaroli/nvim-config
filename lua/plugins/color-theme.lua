724a106e98c4a5feba0dd09c35192c308cda374d
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-03-24 16:24:58 +0200

    Tweak UI

--- lua/plugins/color-theme.lua
+++ lua/plugins/color-theme.lua
@@ -4,7 +4,32 @@ return {
     name = "catppuccin",
     priority = 1000,
     lazy = false,
-    config = function()
+    opts = {
+        transparent_background = true,
+        styles = {
+            sidebars = "transparent",
+            floats = "transparent",
+        },
+        integrations = {
+            cmp = true,
+            gitsigns = true,
+            indent_blankline = {
+                enabled = false,
+                scope_color = "sapphire",
+                colored_indent_levels = false,
+            },
+            mason = true,
+            native_lsp = { enabled = true },
+            notify = true,
+            neotree = true,
+            symbols_outline = true,
+            telescope = true,
+            treesitter = true,
+            treesitter_context = true,
+        },
+    },
+    config = function(_, opts)
+        require("catppuccin").setup(opts)
         vim.cmd.colorscheme "catppuccin-macchiato"
         vim.cmd.hi "Comment gui=none"
     end,
---
+++ lua/plugins/dressing.lua
@@ -0,0 +1,5 @@
+return {
+    "stevearc/dressing.nvim",
+    tag = "v2.2.0",
+    opts = {},
+}
