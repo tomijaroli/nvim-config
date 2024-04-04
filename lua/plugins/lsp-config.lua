4d7c598725db7580d921a5d00032452b22dde525
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-04-04 15:53:05 +0300

    Add fidget config to customize window

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -78,7 +78,23 @@ return {
         {
             "j-hui/fidget.nvim",
             version = "v1.4.0",
-            opts = {},
+            event = "VeryLazy",
+            opts = {
+                notification = {
+                    window = {
+                        normal_hl = "String",
+                        winblend = 0,
+                        border = "rounded",
+                        zindex = 45,
+                        max_width = 0,
+                        max_height = 0,
+                        x_padding = 1,
+                        y_padding = 1,
+                        align = "bottom",
+                        relative = "editor",
+                    },
+                },
+            },
         },
     },
     config = function()
