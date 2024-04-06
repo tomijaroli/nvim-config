1b3c2c70a8731ab02e144df148eff9d9bdd72517
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-04-06 16:10:56 +0300

    Change gitsigns symbols to default

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -3,15 +3,7 @@ return {
     {
         "lewis6991/gitsigns.nvim",
         config = function()
-            require("gitsigns").setup {
-                signs = {
-                    add = { text = "+" },
-                    change = { text = "~" },
-                    delete = { text = "_" },
-                    topdelete = { text = "‾" },
-                    changedelete = { text = "~" },
-                },
-            }
+            require("gitsigns").setup {}
 
             vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
             vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
