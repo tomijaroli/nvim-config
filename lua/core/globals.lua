6d29f77f7997ee194d3179d279214405ddf0902c
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-04-06 14:52:14 +0300

    Cleanup globals

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -1,13 +1,8 @@
-local g = vim.g
-
-g.mapleader = " "
-g.maplocalleader = " "
-
-g.loaded = 1
-g.loaded_netrw = 1
-g.loaded_netrwPlugin = 1
-g.autopairs_enabled = true
-
-g.have_nerd_font = true
-
-g.mkdp_auto_start = 1
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.vim.g.loaded = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.autopairs_enabled = true
+vim.vim.g.have_nerd_font = true
+vim.vim.g.mkdp_auto_start = 1
