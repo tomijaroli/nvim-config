5d917b14aa6ab616b408204c237fec786b2033f8
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-04-06 16:11:08 +0300

    Add diagnostics icons

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -96,6 +96,14 @@ return {
         capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
         local util = require "lspconfig.util"
 
+        -- Change the Diagnostic symbols in the sign column (gutter)
+        -- (not in youtube nvim video)
+        local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+        for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+        end
+
         local servers = {
             clangd = {},
             cssls = {},
