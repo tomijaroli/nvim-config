409e4437cfb979effeb09ffe8033dd5dd2d9921f
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-06-25 08:54:47 +0300

    Fix live grep in hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -22,10 +22,19 @@ return {
                     path_display = { "smart" },
                 },
                 ensure_installed = { "swift" },
+                pickers = {
+                    live_grep = {
+                        file_ignore_patterns = { "node_modules", ".git", ".venv" },
+                        additional_args = function(_)
+                            return { "--hidden" }
+                        end,
+                    },
+                },
                 extensions = {
                     ["ui-select"] = {
                         require("telescope.themes").get_dropdown(),
                     },
+                    "fzf",
                 },
             }
 
@@ -45,12 +54,6 @@ return {
             vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
             vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
             vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-            vim.keymap.set(
-                "n",
-                "<leader>sG",
-                "<cmd>Telescope live_grep hidden=true<CR>",
-                { desc = "[S]earch by [G]rep including hidden" }
-            )
             vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
             vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
             vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
