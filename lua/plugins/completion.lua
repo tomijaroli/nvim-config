5938ded19ba8e4dab7dd2404fc6a098249954928
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-08-06 18:27:47 +0300

    Refactor completion config

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,101 +1,37 @@
 return {
-    {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
+  {
+    "hrsh7th/nvim-cmp",
+    lazy = false,
+    priority = 100,
+    dependencies = {
+      "onsails/lspkind.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      {
+        "L3MON4D3/LuaSnip",
+        build = "make install_jsregexp",
         dependencies = {
-            {
-                "L3MON4D3/LuaSnip",
-                build = (function()
-                    if vim.fn.has "win32" == 1 or vim.fn.executable "make" == 0 then
-                        return
-                    end
-                    return "make install_jsregexp"
-                end)(),
-                dependencies = {
-                    {
-                        "rafamadriz/friendly-snippets",
-                        config = function()
-                            require("luasnip.loaders.from_vscode").lazy_load()
-                        end,
-                    },
-                },
-            },
-            "saadparwaiz1/cmp_luasnip",
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "onsails/lspkind.nvim",
+          {
+            "rafamadriz/friendly-snippets",
+            config = function()
+              require("luasnip.loaders.from_vscode").lazy_load()
+            end,
+          },
         },
-        config = function()
-            local cmp = require "cmp"
-            local luasnip = require "luasnip"
-            local lspkind = require "lspkind"
-
-            luasnip.config.setup {}
-
-            cmp.setup {
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                completion = { completeopt = "menu,menuone,noinsert" },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                mapping = cmp.mapping.preset.insert {
-                    ["<C-p>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-                    ["<C-n>"] = cmp.mapping.select_next_item(), -- next suggestion
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-space>"] = cmp.mapping.complete {}, -- show completion suggestion
-                    ["<C-y>"] = cmp.mapping.confirm { select = false, behavior = cmp.ConfirmBehavior.Replace },
-                    ["<C-l>"] = cmp.mapping(function()
-                        if luasnip.expand_or_locally_jumpable() then
-                            luasnip.expand_or_jump()
-                        end
-                    end, { "i", "s" }),
-                    ["<C-h>"] = cmp.mapping(function()
-                        if luasnip.locally_jumpable(-1) then
-                            luasnip.jump(-1)
-                        end
-                    end, { "i", "s" }),
-                },
-                sources = cmp.config.sources {
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                    { name = "buffer" },
-                    { name = "path" },
-                },
-
-                formatting = {
-                    format = lspkind.cmp_format {
-                        mode = "symbol_text",
-                        menu = {
-                            buffer = "[Buffer]",
-                            nvim_lsp = "[LSP]",
-                            luasnip = "[LuaSnip]",
-                            nvim_lua = "[Lua]",
-                            latex_symbols = "[Latex]",
-                        },
-                        with_text = false,
-                        maxwidth = 50,
-                        ellipsis_char = "...",
-                    },
-                },
-                experimental = {
-                    ghost_text = true,
-                },
-            }
-        end,
+      },
+      "saadparwaiz1/cmp_luasnip",
     },
-    {
-        "olexsmir/gopher.nvim",
-        ft = "go",
-        opts = {},
-        build = function()
-            vim.cmd [[silent! GoInstallDeps]]
-        end,
-    },
+    config = function()
+      require "plugins.config.completion"
+    end,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    opts = {},
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
 }
